25d755bcbdb0ac05d99294b680c75e7bd9d4352b
LSuper <120311070@qq.com>
2023-11-08 23:21:22 +0800

    feat:add version info

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <QApplication>
 #include <QDebug>
 
+#include "version.h"
+
 int main(int argc, char* argv[])
 {
 	QApplication a(argc, argv);
-	qDebug()<<"hello world";
+
+	qDebug()<<QString("The version of this application is v%1").arg(VERSION_STR);
 	return QApplication::exec();
 }
