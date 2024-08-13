9aca7bf17fe93705398ebbe4b10f931fde4d00ae
LSuper <120311070@qq.com>
2024-08-13 22:38:18 +0800

    chore: format main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,17 @@
 #include <QDebug>
 #include <QIcon>
 
-#include "version.h"
 #include "MainWindow.h"
+#include "version.h"
 
 int main(int argc, char* argv[])
 {
-	QApplication a(argc, argv);
+    QApplication a(argc, argv);
     a.setWindowIcon(QIcon(":/resources/logo.svg"));
 
-	qDebug()<<QString("The version of this application is v%1").arg(VERSION_STR);
+    qDebug() << QString("The version of this application is v%1").arg(VERSION_STR);
     MainWindow w;
     w.setWindowTitle("QTemplate " + QString(VERSION_STR));
     w.show();
-	return QApplication::exec();
+    return QApplication::exec();
 }
